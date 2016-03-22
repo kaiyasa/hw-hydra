b14f25186ceb6507ab54789ea99829765668a3c8
Dan Miner <dan.miner@dish.com>
2016-03-22 13:14:16 -0600

    stupid multipler -- no easy bit shifts

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//
+//  r2 = 0
+//  i = r0
+//  while (i-- != 0)
+//      r3 += r1
+//
+
+@R2      // R2 = 0
+M=0
+
+@R0      // i = R0
+D=M
+@i
+M=D
+
+(loop)
+  @end   // if i == 0 goto end
+  D;JEQ
+
+  @R1    // R2 += R1
+  D=M
+  @R2
+  M=D+M
+
+  @i     // D = --i
+  M=M-1
+  D=M
+
+  @loop  // goto loop
+  0;JMP
+
+(end)
+  @end
+  0;JMP
