7aa39d307b8bf00199855a51325946a4083cddd1
Dan Miner <dan.miner@dish.com>
2016-03-23 09:12:30 -0600

    untested but reasonable outline

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -8,4 +8,65 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(INIT)
+  @last      // last = 0
+  M=0
+  @BLANK     // goto BLANK
+  0;JMP
+
+(POLL)
+  @KBD       // tmp = KBD
+  D=M
+  @tmp
+  M=D
+
+  @last     // if ((last - tmp) == 0)
+  D=M-D
+
+  @POLL     // goto POLL
+  D;JEQ
+
+  @tmp      // last = tmp
+  D=M
+  @last
+  M=D
+
+  @BLANK    // if (last == 0) goto BLANK
+  D;JEQ
+
+  @FILL     // goto FILL
+  0;JMP
+
+
+(FILL)
+  @8192     // D=8192
+  D=A
+
+(FLOOP)
+  D=D-1
+  @SCREEN   // SCREEN[D] = -1 (0xFFFF)
+  A=D+A
+  M=-1
+
+  @POLL
+  D;JEQ
+
+  @FLOOP
+  0;JMP
+  
+
+(BLANK)
+  @8192
+  D=A
+
+(BLOOP)
+  D=D-1
+  @SCREEN
+  A=D+A
+  M=0
+
+  @POLL
+  D;JEQ
+
+  @BLOOP
+  0;JMP
